fa1a4a53cacb0e791a5fa52280264bc24558ab16
Etnavas <km6104@nyu.edu>
2025-05-01 00:06:53 -0400

    May 1 Update 1

---
+++ FinalProject/music_website/Library_&_Home/script.js
@@ -0,0 +1,102 @@
+// API module 
+const APIController = (function() {
+    
+    // variables
+    const clientId = 'cfc91860cce347c2a2fecd9521dad10b';
+    const clientSecret = '86ce3d80f10446b8b2b5e72bcb733b48';
+
+    // private methods (can only be accessed from within the class itself)
+    const _getToken = async () => {
+
+        // fetch to network requests, retrieve data from servers or APIs
+        const result = await fetch('https://accounts.spotify.com/api/token', {
+            method: 'POST',
+            headers: {
+                'Content-Type' : 'application/x-www-form-urlencoded', 
+                'Authorization' : 'Basic ' + btoa( clientId + ':' + clientSecret)
+            },
+            body: 'grant_type=client_credentials'
+        });
+
+        const data = await result.json();
+        return data.access_token;
+    }
+
+    // get a list of genres 
+    // Token parametter
+    const _getGenres = async (token) => {
+
+        const result = await fetch(`https://api.spotify.com/v1/browse/categories?locale=sv_US`, {
+            method: 'GET',
+            headers: { 'Authorization' : 'Bearer ' + token}
+        });
+
+        // coverts to json  
+        const data = await result.json();
+        return data.categories.items;
+    }
+
+    // playlists by genre 
+    // token parameter + genre ID parameter 
+    const _getPlaylistByGenre = async (token, genreId) => {
+
+        // variable that holds the limit amountb of play lists 
+        const limit = 10;
+        
+        const result = await fetch(`https://api.spotify.com/v1/browse/categories/${genreId}/playlists?limit=${limit}`, {
+            method: 'GET',
+            headers: { 'Authorization' : 'Bearer ' + token}
+        });
+
+        const data = await result.json();
+        return data.playlists.items;
+    }
+
+    // list of tracks per playlist
+    const _getTracks = async (token, tracksEndPoint) => {
+
+        const limit = 10;
+
+        const result = await fetch(`${tracksEndPoint}?limit=${limit}`, {
+            method: 'GET',
+            headers: { 'Authorization' : 'Bearer ' + token}
+        });
+
+        const data = await result.json();
+        return data.items;
+    }
+
+    // selected track 
+    const _getTrack = async (token, trackEndPoint) => {
+
+        const result = await fetch(`${trackEndPoint}`, {
+            method: 'GET',
+            headers: { 'Authorization' : 'Bearer ' + token}
+        });
+
+        const data = await result.json();
+        return data;
+    }
+
+    return {
+        getToken() {
+            return _getToken();
+        },
+        getGenres(token) {
+            return _getGenres(token);
+        },
+        getPlaylistByGenre(token, genreId) {
+            return _getPlaylistByGenre(token, genreId);
+        },
+        getTracks(token, tracksEndPoint) {
+            return _getTracks(token, tracksEndPoint);
+        },
+        getTrack(token, trackEndPoint) {
+            return _getTrack(token, trackEndPoint);
+        }
+    }
+
+
+// IIFE "})()" Funtions fires Imediatly
+})(); 
+
